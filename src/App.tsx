2a42bcc04761afa59501a1f3d66a15f7b71eee19
Eliel Oliveira da Silva <eliel.oliveira2004@gmail.com>
2022-06-11 14:29:45 -0300

    .

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,17 @@ import { Home, Init, AuthPage } from "./pages";
 
 import { AppProvider } from "./contexts/appContext";
 
+
 const App: React.FC = () => {
   return (
     <AppProvider>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Init />} />
-          <Route path="/auth/login" element={<AuthPage />} />
-          <Route path="/auth/sign-up" element={<AuthPage />} />
-          <Route path="/homepage" element={<Home />} />
-          <Route path="*" element={<Init />} />
+          <Route path="" element={<Init />} />
+          <Route path="auth/login" element={<AuthPage />} />
+          <Route path="auth/sign-up" element={<AuthPage />} />
+          <Route path="homepage" element={<Home />} />
+          <Route path="/*" element={<Init />} />
         </Routes>
       </BrowserRouter>
     </AppProvider>
