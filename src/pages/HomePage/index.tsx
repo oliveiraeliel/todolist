6872ba17e17d28af5896dac5536b99e4d6ec6109
Eliel Oliveira da Silva <eliel.oliveira2004@gmail.com>
2022-06-12 22:57:02 -0300

    building navbar

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
+import { SpecialLetter } from "../../components";
+
+import AppContext from "../../contexts/appContext";
+import { NavBar, Main } from "./styles";
 
 const Home: React.FC = () => {
-  return <div></div>;
+  const { signed, user } = useContext(AppContext);
+
+  return (
+    <div>
+      <NavBar>
+        <SpecialLetter text="TODO.LIST" style={{ fontSize: "5rem" }} />
+        {user !== null ? user.name : "create account"}
+      </NavBar>
+      <Main></Main>
+    </div>
+  );
 };
 
 export default Home;
--- src/pages/HomePage/styles.tsx
+++ src/pages/HomePage/styles.tsx
@@ -1,1 +1,13 @@
 import styled from "styled-components";
+
+export const NavBar = styled.nav`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0rem 5rem 0 5rem;
+`;
+
+export const Main = styled.main`
+  display: flex;
+`;
